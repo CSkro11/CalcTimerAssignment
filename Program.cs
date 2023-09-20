78cbeaff0895b107a98d8e3aac72b50f2bd7b604
Conor <c_skrobot@yahoo.com>
2023-09-20 00:25:15 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello MEE 381");
+
+        float a = 0.72f;
+        float b = 0.33f;
+        float c = 1.05f;
+
+        if(a+b == c){
+            Console.WriteLine("they are equal.");
+        }
+        else{
+            Console.WriteLine("They are not equal");
+    
+        }
+        Console.WriteLine("a{6:6}",a);
+
+
+
+
+
+    }
+}
